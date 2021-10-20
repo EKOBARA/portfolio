23d3452c6dd852a0dd27bb713cf585238342a0bb
ekobara <emmanuelkobara@gmail.com>
2021-10-19 21:56:51 -0400

    Added Project links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 const Projects = () => {
     return (
-        <div className='Projects'>
-            <section>Project 1</section>
-            <section>Project 2</section>
-            <section>Project 3</section>
-            <section>Project 4</section>
-        </div>
-    );
+			<div className='Projects'>
+				<section>
+					<a href='https://ekobara.github.io/guess-the-region/'>
+						Guess The Region
+					</a>
+				</section>
+				<section>
+					<a href='https://optimistic-murdock-828ba3.netlify.app/'>
+						Views From Mars
+					</a>
+				</section>
+				<section>
+					<a href='https://pricelessapp.netlify.app/'>
+                        Priceless
+                    </a>
+				</section>
+				<section>Project 4</section>
+			</div>
+		);
 };
 
 export default Projects;
