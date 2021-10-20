208cbe6a81cb6ac51c42fafb0672118684d84add
ekobara <emmanuelkobara@gmail.com>
2021-10-19 22:28:26 -0400

    Added Styling to About component

--- src/components/About.js
+++ src/components/About.js
@@ -4,19 +4,18 @@ import Contact from './Contact';
 
 const About = () => {
     return (
-        <div classname='home'>
-            <section classname='info'>
-                I am a full stack software developer with a passion for innovation. I am driven by the desire to help create and drive the technological wheel to move society forward. From building user friendly applications to help make lives easier to finding ways to improve established works, I love finding creative ways to solve problems.
-            </section>
+        <div className='about'>
+            <div >
+                <p className='info'>
+                    I am a full stack software developer with a passion for innovation. I am driven by the desire to help create and drive the technological wheel to move society forward. From building user friendly applications to help make lives easier to finding ways to improve established works, I love finding creative ways to solve problems.
+                </p>
+            </div>
             <div>
                <Link to='/projects'>
                     <h3>
                         Projects
                     </h3>
                </Link>
-            </div>
-            <div>
-                insert link to resume here
             </div>
             <Contact />
         </div>
