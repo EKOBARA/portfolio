ec0024ecfb07de13c3e87991ea7e6d579be085fe
ekobara <emmanuelkobara@gmail.com>
2021-11-01 23:48:33 -0400

    Css added to projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import { Card, CardGroup, CardImg } from 'react-bootstrap';
+import Mars from '../assets/mars.png';
+import Region from '../assets/region.png';
+import Price from '../assets/priceless.png';
 
 const Projects = () => {
     return (
 			<div className='Projects'>
-				<CardGroup>
-					<a href='https://ekobara.github.io/guess-the-region/'>
+				<li className='pro'>
+					<a classname='title' href='https://ekobara.github.io/guess-the-region/'>
 						Guess The Region
 					</a>
-					<CardImg src="../images/Screen Shot 2021-09-15 at 5.51.38 PM.png" alt="Guess the Region" />
-				</CardGroup>
-				<CardGroup>
-					<a href='https://optimistic-murdock-828ba3.netlify.app/'>
+					<img src={Region} alt="Guess the Region" />
+				</li>
+				<li className='pro'>
+					<a classname='title' href='https://viewsfrommars.netlify.app/'>
 						Views From Mars
 					</a>
-						<CardImg src="../images/Screen Shot 2021-09-23 at 11.35.54 AM.png" alt="Views From Mars" />
-				</CardGroup>
-				<CardGroup>
-					<a href='https://pricelessapp.netlify.app/'>
+						<img src={Mars} alt="Views From Mars" />
+				</li>
+				<li className='pro'>
+					<a classname='title' href='https://pricelessapp.netlify.app/'>
                         Priceless
                     </a>
-					<CardImg src="../images/Screen Shot 2021-11-01 at 8.50.36 AM.png" alt="Priceless" />
-				</CardGroup>
-				{/* <section>Project 4</section> */}
+					<img src={Price} alt="Priceless" />
+				</li>
 			</div>
 		);
 };
